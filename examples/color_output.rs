6d0fb514c8e9097ce9dc69eded51e452c5e5eb79
Tao Guo <tao.guo.code@gmail.com>
2020-07-24 14:46:58 -0700

    Add color_ouput example
    Make sure the FunResult could also hold invisible strings

---
+++ examples/color_output.rs
@@ -0,0 +1,13 @@
+use cmd_lib::{ CmdResult, run_cmd, run_fun };
+
+fn main() -> CmdResult {
+    let red = run_fun!(tput setaf 1)?;
+    let green = run_fun!(tput setaf 2)?;
+    let reset = run_fun!(tput sgr0)?;
+    run_cmd!(|red, green, reset| /bin/echo "1: ${red}red text ${green}green text${reset}")?;
+    println!("2: \x1b[0;31mred text \x1b[0;32mgreen text\x1b[0m");
+    // rust tokenstream does not allow this:
+    // run_cmd!(bash -c 'echo -ne "\x1b[0;31mred text \x1b[0;32mgreen text\x1b[0m"');
+    run_cmd!(bash -c "echo -ne '3: \x1b[0;31mred text \x1b[0;32mgreen text\x1b[0m'")?;
+    Ok(())
+}
