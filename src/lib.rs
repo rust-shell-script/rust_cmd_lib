1675fe170e8ef761f7d22bff60d3768869340873
Tao Guo <tao.guo.code@gmail.com>
2021-02-19 20:53:41 -0800

    Revert "remove dependency to cmd_lib_core"
    This reverts commit 68e00a1023700d204c3e30db408cf0a45a3ea52a.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,10 @@
-pub use cmd_lib_core;
-pub use cmd_lib_macros;
+use cmd_lib_core;
+use cmd_lib_macros;
 
-#[macro_export]
-macro_rules! run_cmd {
-    ($($cur:tt)*) => {{
-	use $crate::cmd_lib_core;
-	$crate::cmd_lib_macros::run_cmd!($($cur)*)
-    }};
-}
-
-#[macro_export]
-macro_rules! run_fun {
-    ($($cur:tt)*) => {{
-	use $crate::cmd_lib_core;
-	$crate::cmd_lib_macros::run_fun!($($cur)*)
-    }};
-}
+pub use cmd_lib_macros::{
+    run_cmd,
+    run_fun,
+};
 
 pub use cmd_lib_core::{
     run_cmd,
