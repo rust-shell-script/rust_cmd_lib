7918699ee968afe503ddbe91de1fd1c888437927
Tao Guo <tao.guo.code@gmail.com>
2021-03-01 09:34:07 -0800

    Fix doc test failure

--- src/lib.rs
+++ src/lib.rs
@@ -144,17 +144,14 @@
 //! Declare your function with `export_cmd` attribute:
 //!
 //! ```
-//! use cmd_lib::{export_cmd, CmdArgs, CmdEnvs, FunResult};
+//! use cmd_lib::{export_cmd, use_cmd, run_cmd, run_fun, CmdArgs, CmdEnvs, FunResult};
 //! #[export_cmd(my_cmd)]
 //! fn foo(args: CmdArgs, _envs: CmdEnvs) -> FunResult {
 //!     println!("msg from foo(), args: {:?}", args);
 //!     Ok("bar".into())
 //! }
-//! ```
 //!
-//! To use it, just import it at first:
-//! ```compile_fail
-//! use cmd_lib::{use_cmd, run_cmd, run_fun};
+//! // To use it, just import it at first:
 //! use_cmd!(my_cmd);
 //! run_cmd!(my_cmd).unwrap();
 //! println!("get result: {}", run_fun!(my_cmd).unwrap());
