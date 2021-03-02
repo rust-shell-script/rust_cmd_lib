67208a87cdd14e83979c210cf6fc0ab048e551f4
Tao Guo <tao.guo.code@gmail.com>
2021-03-02 07:19:09 -0800

    Hide builtin functions

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,9 +1,11 @@
 use crate::{CmdArgs, CmdEnvs, FunResult};
 
+#[doc(hidden)]
 pub fn builtin_echo(args: CmdArgs, _envs: CmdEnvs) -> FunResult {
     Ok(args[1..].join(" "))
 }
 
+#[doc(hidden)]
 pub fn builtin_true(_args: CmdArgs, _envs: CmdEnvs) -> FunResult {
     Ok("".into())
 }
