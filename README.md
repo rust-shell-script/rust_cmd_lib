47c81092e41db0d28ac31ab18337d149fc7d4b8b
Tao Guo <tao.guo.code@gmail.com>
2025-12-08 23:15:21 +0000

    update README

--- README.md
+++ README.md
@@ -32,47 +32,6 @@ they can usually be implemented as one line of rust macro with the help of this 
 [examples/rust_cookbook.rs](https://github.com/rust-shell-script/rust_cmd_lib/blob/master/examples/rust_cookbook.rs).
 Since they are rust code, you can always rewrite them in rust natively in the future, if necessary without spawning external commands.
 
-### What this library looks like
-
-To get a first impression, here is an example from
-[examples/dd_test.rs](https://github.com/rust-shell-script/rust_cmd_lib/blob/master/examples/dd_test.rs):
-
-```rust
-run_cmd! (
-    info "Dropping caches at first";
-    sudo bash -c "echo 3 > /proc/sys/vm/drop_caches";
-    info "Running with thread_num: $thread_num, block_size: $block_size";
-)?;
-let cnt = DATA_SIZE / thread_num / block_size;
-let now = Instant::now();
-(0..thread_num).into_par_iter().for_each(|i| {
-    let off = cnt * i;
-    let bandwidth = run_fun!(
-        sudo bash -c "dd if=$file of=/dev/null bs=$block_size skip=$off count=$cnt 2>&1"
-        | awk r#"/copied/{print $(NF-1) " " $NF}"#
-    )
-    .unwrap_or_else(|_| cmd_die!("thread $i failed"));
-    info!("thread {i} bandwidth: {bandwidth}");
-});
-let total_bandwidth = Byte::from_bytes((DATA_SIZE / now.elapsed().as_secs()) as u128).get_appropriate_unit(true);
-info!("Total bandwidth: {total_bandwidth}/s");
-```
-
-Output will be like this:
-
-```console
-➜  rust_cmd_lib git:(master) ✗ cargo run --example dd_test -- -b 4096 -f /dev/nvme0n1 -t 4
-    Finished dev [unoptimized + debuginfo] target(s) in 0.04s
-     Running `target/debug/examples/dd_test -b 4096 -f /dev/nvme0n1 -t 4`
-[INFO ] Dropping caches at first
-[INFO ] Running with thread_num: 4, block_size: 4096
-[INFO ] thread 3 bandwidth: 317 MB/s
-[INFO ] thread 1 bandwidth: 289 MB/s
-[INFO ] thread 0 bandwidth: 281 MB/s
-[INFO ] thread 2 bandwidth: 279 MB/s
-[INFO ] Total bandwidth: 1.11 GiB/s
-```
-
 ### What this library provides
 
 #### Macros to run external commands
@@ -293,6 +252,10 @@ let d = tls_get!(DELAY);
 ```
 
 ### Other Notes
+
+#### Minimum supported Rust version
+
+cmd_lib(2.0)'s MSRV is 1.88.
 
 #### Environment Variables
 
--- src/lib.rs
+++ src/lib.rs
@@ -30,56 +30,6 @@
 //! [examples/rust_cookbook.rs](https://github.com/rust-shell-script/rust_cmd_lib/blob/master/examples/rust_cookbook.rs).
 //! Since they are rust code, you can always rewrite them in rust natively in the future, if necessary without spawning external commands.
 //!
-//! ## What this library looks like
-//!
-//! To get a first impression, here is an example from
-//! [examples/dd_test.rs](https://github.com/rust-shell-script/rust_cmd_lib/blob/master/examples/dd_test.rs):
-//!
-//! ```no_run
-//! # use byte_unit::Byte;
-//! # use cmd_lib::*;
-//! # use rayon::prelude::*;
-//! # use std::time::Instant;
-//! # const DATA_SIZE: u64 = 10 * 1024 * 1024 * 1024; // 10GB data
-//! # let mut file = String::new();
-//! # let mut block_size: u64 = 4096;
-//! # let mut thread_num: u64 = 1;
-//! run_cmd! (
-//!     info "Dropping caches at first";
-//!     sudo bash -c "echo 3 > /proc/sys/vm/drop_caches";
-//!     info "Running with thread_num: $thread_num, block_size: $block_size";
-//! )?;
-//! let cnt = DATA_SIZE / thread_num / block_size;
-//! let now = Instant::now();
-//! (0..thread_num).into_par_iter().for_each(|i| {
-//!     let off = cnt * i;
-//!     let bandwidth = run_fun!(
-//!         sudo bash -c "dd if=$file of=/dev/null bs=$block_size skip=$off count=$cnt 2>&1"
-//!         | awk r#"/copied/{print $(NF-1) " " $NF}"#
-//!     )
-//!     .unwrap_or_else(|_| cmd_die!("thread $i failed"));
-//!     info!("thread {i} bandwidth: {bandwidth}");
-//! });
-//! let total_bandwidth = Byte::from_bytes((DATA_SIZE / now.elapsed().as_secs()) as u128).get_appropriate_unit(true);
-//! info!("Total bandwidth: {total_bandwidth}/s");
-//! # Ok::<(), std::io::Error>(())
-//! ```
-//!
-//! Output will be like this:
-//!
-//! ```console
-//! ➜  rust_cmd_lib git:(master) ✗ cargo run --example dd_test -- -b 4096 -f /dev/nvme0n1 -t 4
-//!     Finished dev [unoptimized + debuginfo] target(s) in 0.04s
-//!      Running `target/debug/examples/dd_test -b 4096 -f /dev/nvme0n1 -t 4`
-//! [INFO ] Dropping caches at first
-//! [INFO ] Running with thread_num: 4, block_size: 4096
-//! [INFO ] thread 3 bandwidth: 317 MB/s
-//! [INFO ] thread 1 bandwidth: 289 MB/s
-//! [INFO ] thread 0 bandwidth: 281 MB/s
-//! [INFO ] thread 2 bandwidth: 279 MB/s
-//! [INFO ] Total bandwidth: 1.11 GiB/s
-//! ```
-//!
 //! ## What this library provides
 //!
 //! ### Macros to run external commands
@@ -323,6 +273,10 @@
 //! ```
 //!
 //! ## Other Notes
+//!
+//! ### Minimum supported Rust version
+//!
+//! cmd_lib(2.0)'s MSRV is 1.88.
 //!
 //! ### Environment Variables
 //!
